eda2166217970483cc2aa49992fe75370cf5b640
John Grishin <r@exah.me>
2023-10-23 10:16:14 +0100

    ⬆️ Update `next.js`

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,48 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="@emotion/react/types/css-prop" />
+/// <reference types="next/image-types/global" />
 
-declare module '*.md' {
-  export default () => JSX.Element
-}
-
-declare module '@mdx-js/react' {
-  import * as React from 'react'
-
-  type ComponentType =
-    | 'a'
-    | 'blockquote'
-    | 'code'
-    | 'delete'
-    | 'em'
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'h4'
-    | 'h5'
-    | 'h6'
-    | 'hr'
-    | 'img'
-    | 'inlineCode'
-    | 'li'
-    | 'ol'
-    | 'p'
-    | 'pre'
-    | 'strong'
-    | 'sup'
-    | 'table'
-    | 'td'
-    | 'thematicBreak'
-    | 'tr'
-    | 'ul'
-
-  export type Components = {
-    [K in ComponentType]?: React.ComponentType<{ children: React.ReactNode }>
-  }
-  export interface MDXProviderProps {
-    children: React.ReactNode
-    components: Components
-  }
-
-  export class MDXProvider extends React.Component<MDXProviderProps> {}
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
